2076d4e7adcb75623ffc07505914858bf384ad82
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-02-08 12:28:58 +0800

    new conf

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-for i in `seq 1 5`; do
+for i in `seq 1 4`; do
 	echo "Train ./conf/PNN1_"${i}".conf"
 	python criteo.py --train --gpu 0 --conf_path ./conf/PNN1_${i}.conf 1>criteo_train_${i}.log 2>&1
 	echo "Train ./conf/PNN1_"${i}".conf Finish"

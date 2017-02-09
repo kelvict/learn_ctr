6379d497ac41a3dd38a4405ed8cc9f69b4fbdcad
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-02-09 17:36:55 +0800

    Add xgb feat

--- criteo.py
+++ criteo.py
@@ -17,6 +17,8 @@ if __name__ == "__main__":
 	parser.add_argument("--continue_min_freq", type=int,default=20)
 	parser.add_argument("--continue_n_interval", type=int,default=1000)
 	parser.add_argument("--split_by_field", action="store_true", help="if split by field")
+	parser.add_argument("--add_xgb_feat", action="store_true", help="add xgboost feature")
+	parser.add_argument("--drop_contin_feat", action="store_true", help="drop contine feature")
 	#Split trainset and testset
 	parser.add_argument("--split_train_test", action="store_true")
 	parser.add_argument("--dataset_path", type=str,)
@@ -41,7 +43,7 @@ if __name__ == "__main__":
 		input_path = args.input
 		criteo_preprocesser.preprocess(
 			input_path, args.test, args.split_num, args.discrete_min_freq, args.continue_n_interval, args.continue_min_freq,
-			split_by_field=args.split_by_field)
+			split_by_field=args.split_by_field, add_xgb_feat=args.add_xgb_feat, drop_contin_feat=args.drop_contin_feat)
 		print "Finish Preprocessing"
 	elif args.train:
 		import os
@@ -75,11 +77,11 @@ if __name__ == "__main__":
 			from preprocesser import criteo_preprocesser
 			input_path = "dataset/ctr/criteo/dac_sample/dac_sample.csv"
 			criteo_preprocesser.preprocess(
-				input_path, False, 2, 1000, 1000, 1000,split_by_col=True)
+				input_path, False, 2, 1000, 1000, 1000,split_by_field=True, add_xgb_feat=False, drop_contin_feat=False)
 			print "Finish Preprocessing"
 		elif mode == 1:
 			from train import criteo_train
-			str = "./conf/FNN.conf;./conf/LR.conf;./conf/PNN1.conf;./conf/FM.conf;./conf/CCPM.conf;./conf/PNN2.conf"
+			str = "./conf/test_LR_no_contin.conf"
 			conf_paths = str.split(";")
 			for conf_path in conf_paths:
 				criteo_train.train_model_with_conf(conf_path)
@@ -91,11 +93,17 @@ if __name__ == "__main__":
 		elif mode == 3:
 			from util import preprocess, log
 			dataset_path = "dataset/ctr/criteo/dac_sample/dac_sample.csv.20170122_155305.discrete_1000_contin_1000_1000.all_split_by_col_csr_mats.pkl"
+			disc_contin_path = "dataset/ctr/criteo/dac_sample/dac_sample.csv.20170209_163314.discrete_1000_26_contin_1000_1000_13_xgb_0.all_split_by_field_csr_mats.pkl"
+			disc_xgb_path = "dataset/ctr/criteo/dac_sample/dac_sample.csv.20170209_162932.discrete_1000_26_contin_1000_1000_0_xgb_15.all_split_by_field_csr_mats.pkl"
+			disc_contin_xgb_path = "dataset/ctr/criteo/dac_sample/dac_sample.csv.20170209_162704.discrete_1000_26_contin_1000_1000_13_xgb_15.all_split_by_field_csr_mats.pkl"
+			labels_path = "dataset/ctr/criteo/dac_sample/dac_sample.csv.20170209_161536.labels.txt"
 			log.config_log("./log/split_trainset_testset_%s.log"%(dataset_path.replace('/', '_')))
-			preprocess.split_train_test_data(
-				"dataset/ctr/criteo/dac_sample/dac_sample.csv.20170122_155305.discrete_1000_contin_1000_1000.all_split_by_col_csr_mats.pkl",
-				"dataset/ctr/criteo/dac_sample/dac_sample.csv.20170122_155305.labels.txt",
-				0.75,
-				"dataset/ctr/criteo/dac_sample/dac_sample.csv.20170122_155305.discrete_1000_contin_1000_1000.all_split_by_col_csr_mats_train.pkl",
-				"dataset/ctr/criteo/dac_sample/dac_sample.csv.20170122_155305.discrete_1000_contin_1000_1000.all_split_by_col_csr_mats_test.pkl")
+			for path in [disc_contin_path, disc_xgb_path, disc_contin_xgb_path]:
+				print "Split path %s"%path
+				preprocess.split_train_test_data(
+					path,
+					labels_path,
+					0.75,
+					path+".train.pkl",
+					path+".test.pkl")
 

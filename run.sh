c48682bb88690cf89ba9674da042a5425dc208f5
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-02-10 21:49:48 +0800

    Add conf

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ for i in `seq 1 1`; do
 	echo "Train ./conf/PNN1_"${i}"_xgb_contin.conf Finish"
 
 	echo "Train ./conf/PNN1_"${i}"_xgb_no_contin.conf"
-	python criteo.py --train --gpu 0 --conf_path ./conf/PNN1_${i}_xgb_no_contin.conf 1>criteo_train_${i}.log 2>&1
+	python criteo.py --train --gpu 0 --conf_path ./conf/PNN1_${i}_xgb_no_contin.conf 1>criteo_train_${i}_2.log 2>&1
 	echo "Train ./conf/PNN1_"${i}"_xgb_no_contin.conf Finish"
 
 done

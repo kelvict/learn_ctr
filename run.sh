a56d8bfb1d50c26a840e35faf9208fa41d24d5b2
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-02-15 11:48:13 +0800

    fix sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-for i in `seq 1 2`; do
+for i in `seq 2 2`; do
 	echo "Train ./conf/PNN1_"${i}"_40xgb_no_contin.conf"
 	python criteo.py --train --gpu 0 --conf_path ./conf/PNN1_${i}_40xgb_no_contin.conf 1>criteo_train_${i}_no_xgb.log 2>&1
 	echo "Train ./conf/PNN1_"${i}"_40xgb_no_contin.conf Finish"

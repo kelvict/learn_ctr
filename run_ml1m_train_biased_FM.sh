bbc419bd43b4df66890d695f1da156c762d77ead
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-02-25 23:27:09 +0800

    Fix conf

--- run_ml1m_train_biased_FM.sh
+++ run_ml1m_train_biased_FM.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-python criteo.py --ml --train  --gpu 1 --conf_path ./conf/RecIPNN_rate_biased_MF.conf 1>log/ml1m_train_rate_biased_MF_sparse.log 2>&1&
+python criteo.py --ml --train  --gpu 0 --conf_path ./conf/RecIPNN_rate_biased_MF.conf 1>log/ml1m_train_rate_biased_MF.log 2>&1&
 sleep 2s
-python criteo.py --ml --train  --gpu 1 --conf_path ./conf/RecIPNN_rate_biased_MF_sparse.conf 1>log/ml1m_train_rate_biased_MF_sparse.log 2>&1&
+python criteo.py --ml --train  --gpu 0 --conf_path ./conf/RecIPNN_rate_biased_MF_sparse.conf 1>log/ml1m_train_rate_biased_MF_sparse.log 2>&1&

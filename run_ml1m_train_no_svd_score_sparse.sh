818b663d0c18bee1c50f1d579e08d8b2d996db6f
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-02-27 19:30:38 +0800

    change lr

--- run_ml1m_train_no_svd_score_sparse.sh
+++ run_ml1m_train_no_svd_score_sparse.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 for i in `seq 21 21`; do
-	python criteo.py --ml --train  --gpu 1 --conf_path ./conf/RecIPNN_rate_time_user_movie_no_svd_score_${i}_sparse.conf 1>log/ml1m_train_rate_time_user_movie_no_svd_score_${i}_sparse.log 2>&1&
+	python criteo.py --ml --train  --gpu 0 --conf_path ./conf/RecIPNN_rate_time_user_movie_no_svd_score_${i}_sparse.conf 1>log/ml1m_train_rate_time_user_movie_no_svd_score_${i}_sparse.log 2>&1&
 	sleep 2s
 done

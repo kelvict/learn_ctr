86c8485e82ad75c9b4da3d8696cd265b17175697
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-03-02 00:04:57 +0800

    Add conf

--- run_ml1m_train_pre_rate_no_svd_score.sh
+++ run_ml1m_train_pre_rate_no_svd_score.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-for i in `seq 11 18`; do
+for i in `seq 1 20`; do
 	python criteo.py --ml --train  --gpu 1 --conf_path ./conf/RecIPNN_rate_time_user_movie_pre_rate_no_svd_score_${i}.conf 1>log/ml1m_train_rate_time_user_movie_pre_rate_no_svd_score_${i}.log 2>&1&
 	sleep 2s
 done

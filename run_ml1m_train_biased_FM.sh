f5b2a3a01443fc4304b7c3657ff4a19797fae0a1
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-03-02 13:32:53 +0800

    Add conf

--- run_ml1m_train_biased_FM.sh
+++ run_ml1m_train_biased_FM.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-python criteo.py --ml --train  --gpu 0 --conf_path ./conf/RecIPNN_rate_biased_MF.conf 1>log/ml1m_train_rate_biased_MF.log 2>&1&
-sleep 2s
-python criteo.py --ml --train  --gpu 0 --conf_path ./conf/RecIPNN_rate_biased_MF_sparse.conf 1>log/ml1m_train_rate_biased_MF_sparse.log 2>&1&
+for i in `seq 1 5`; do
+	python criteo.py --ml --train  --gpu 0 --conf_path ./conf/RecIPNN_rate_biased_MF_${i}.conf 1>log/ml1m_train_rate_biased_MF_${i}.log 2>&1&
+	sleep 2s
+done

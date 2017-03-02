176ee7cf3277d3eb7c04fe442b9cb4bef1b059cd
kelvict <405630376@qq.com>
2017-03-02 19:10:28 +0800

    Fix conflix

--- run_ml1m_train_no_svd_score.sh
+++ run_ml1m_train_no_svd_score.sh
@@ -4,7 +4,7 @@ for conf_id in 51; do
 		for random_seed in `seq 0 4`; do
 			for trainset_rate in 0.9; do
 				for width in 10 15; do
-					python criteo.py --ml --train  --gpu 0 \
+					python criteo.py --ml --train  --gpu 1 \
 					--conf_path ./conf/RecIPNN_rate_time_user_movie_no_svd_score_${conf_id}.conf \
 					--params --width ${width} --lr ${lr} --${random_seed}_${trainset_rate} \
 					1>log/ml1m_train_rate_time_user_movie_no_svd_score_${conf_id}.log 2>&1&

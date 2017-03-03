3374393ccb45a7b60c1be6bf63858947841167e9
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-03-04 01:42:13 +0800

    Add conf

--- run_ml1m_train_pre_rate_no_svd_score.sh
+++ run_ml1m_train_pre_rate_no_svd_score.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 for conf_id in 41 42 43 44; do
-	for lr in 0.0002; do
+	for lr in 0.0001 0.0003; do
 		for random_seed in `seq 0 2`; do
 			for trainset_rate in 0.9; do
 				for width in 5 10; do
 					for n_embd in 75; do
-						for reg in 0.001 0.05; do
-							python criteo.py --ml --train  --gpu 1 \
+						for reg in 0.002; do
+							python criteo.py --ml --train  --gpu 0 \
 							--conf_path ./conf/RecIPNN_rate_time_user_movie_pre_rate_no_svd_score_${conf_id}.conf \
 							--params --width ${width} --lr ${lr} --n_embd ${n_embd} --reg ${reg}\
 							--data_suffix .${random_seed}_${trainset_rate} \

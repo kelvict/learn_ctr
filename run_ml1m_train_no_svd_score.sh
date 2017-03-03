8309515caded318120494c4998c7862c8b914ed5
kelvict <405630376@qq.com>
2017-03-03 15:43:10 +0800

    commit my commit

--- run_ml1m_train_no_svd_score.sh
+++ run_ml1m_train_no_svd_score.sh
@@ -3,8 +3,8 @@ for conf_id in 50; do
 	for lr in 0.0001; do
 		for random_seed in `seq 0 4`; do
 			for trainset_rate in 0.9; do
-				for width in 5 8 10; do
-					for n_embd in 100; do
+				for width in 10; do
+					for n_embd in 90 110; do
 						for reg in 0.001; do
 							python criteo.py --ml --train  --gpu 1 \
 							--conf_path ./conf/RecIPNN_rate_time_user_movie_no_svd_score_${conf_id}.conf \

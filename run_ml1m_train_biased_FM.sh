b24014431dc7fff98ed555a7af913a363698ca0f
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-03-05 22:49:15 +0800

    Test rmse

--- run_ml1m_train_biased_FM.sh
+++ run_ml1m_train_biased_FM.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 for conf_id in 1; do
-	for lr in 0.02; do
+	for lr in 0.02 0.002; do
 		for random_seed in `seq 0 0`; do
 			for trainset_rate in 0.9; do
 				for n_embd in 50 ; do #100 is best now
-					for reg in 0.1; do #0.001 is best now
+					for reg in 0.1 0.01; do #0.001 is best now
 						python criteo.py --ml --train  --gpu 1 \
 						--conf_path ./conf/biased_MF_rate_${conf_id}.conf \
 						--params --lr ${lr} --n_embd ${n_embd} --reg ${reg}\

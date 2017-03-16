9b8f8f75423c178a320099127d3f25fa5b56ee03
kelvict <405630376@qq.com>
2017-03-16 20:12:58 +0800

    test dtype

--- run_yelp_train_review_user_business.sh
+++ run_yelp_train_review_user_business.sh
@@ -2,7 +2,7 @@
 for conf_id in 1; do
 	for lr in 0.0002; do
 		for width in 10; do
-			for n_embd in 100; do
+			for n_embd in 50; do
 				for reg in 0.001; do
 					python criteo.py --ml --train  --gpu 0 \
 					--conf_path ./conf/yelp_RecIPNN_rate_review_user_business_${conf_id}.conf \

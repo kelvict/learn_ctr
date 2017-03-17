6a1aae0305cfae26aa4bf645dd5198163cf977f1
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-03-17 10:56:34 +0800

    Add conf

--- run_yelp_train_only_rate.sh
+++ run_yelp_train_only_rate.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 for conf_id in 1; do
-	for lr in 0.0005; do
+	for lr in 0.0002; do
 		for width in 20; do
-			for n_embd in 30 20 10 75 50 100; do
-				for reg in 0.001; do
+			for n_embd in 75 50 100; do
+				for reg in 0.005 0.0005; do
 					python criteo.py --ml --train  --gpu 1 \
 					--conf_path ./conf/yelp_RecIPNN_rate_${conf_id}.conf \
 					--params --width ${width} --lr ${lr} --n_embd ${n_embd} --reg ${reg}\

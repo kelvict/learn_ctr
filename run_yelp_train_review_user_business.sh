9609427a62d02cb26a2f3da166681e8d6f670aad
kelvict <405630376@qq.com>
2017-03-18 17:38:04 +0800

    Add conf

--- run_yelp_train_review_user_business.sh
+++ run_yelp_train_review_user_business.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 for conf_id in 1; do
 	for lr in 0.001; do
-		for width in 20; do
-			for n_embd in 50; do
+		for width in 10 20; do
+			for n_embd in 50 25; do
 				for reg in 0.001; do
 					python criteo.py --ml --train  --gpu 0 \
 					--conf_path ./conf/yelp_RecIPNN_rate_review_user_business_${conf_id}.conf \
--- run_yelp_train_review_user_business_no_friend.sh
+++ run_yelp_train_review_user_business_no_friend.sh
@@ -2,9 +2,9 @@
 for conf_id in 2 3 1; do
 	for lr in 0.001; do
 		for width in 10 5; do
-			for n_embd in 50; do
+			for n_embd in 75; do
 				for reg in 0.005 0.01; do
-					python criteo.py --ml --train  --gpu 1 \
+					python criteo.py --ml --train  --gpu 0 \
 					--conf_path ./conf/yelp_RecIPNN_rate_review_user_business_no_friend_${conf_id}.conf \
 					--params --width ${width} --lr ${lr} --n_embd ${n_embd} --reg ${reg}\
 					1>./log/yelp_RecIPNN_rate_review_user_business_no_friend_${conf_id}.log 2>&1

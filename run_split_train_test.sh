8000dc2ed919ed9d7f99712ddfcedad6bfc2b19a
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-03-18 17:59:38 +0800

    Add split train test sh

--- run_split_train_test.sh
+++ run_split_train_test.sh
@@ -1,3 +1,4 @@
+:'
 python criteo.py --split_train_test --dataset_path dataset/ctr/criteo/train.txt.20170211_011802.discrete_50_26_contin_100000_2_0_xgb_40.all_split_by_field_csr_mats.pkl \
 --labels_path dataset/ctr/criteo/train.txt.20170211_011802.labels.txt \
 --trainset_rate 0.9 \
@@ -11,3 +12,11 @@ python criteo.py --split_train_test --dataset_path dataset/ctr/criteo/train.txt.
 --traindata_dump_path dataset/ctr/criteo/train.txt.20170211_105206.discrete_50_26_contin_100000_2_13_xgb_40.all_split_by_field_csr_mats.pkl.train.pkl \
 --testdata_dump_path dataset/ctr/criteo/train.txt.20170211_105206.discrete_50_26_contin_100000_2_13_xgb_40.all_split_by_field_csr_mats.pkl.test.pkl \
 > criteo_split_train_test2.log
+'
+python criteo.py --split_train_test --dataset_path dataset/ctr/criteo/train.txt.20170318_132217.discrete_50_26_contin_100000_2_0_xgb_0.all_split_by_field_csr_mats.pkl \
+--labels_path train.txt.20170318_132217.labels.txt \
+--trainset_rate 0.9 \
+--traindata_dump_path dataset/ctr/criteo/train.txt.20170318_132217.discrete_50_26_contin_100000_2_0_xgb_0.all_split_by_field_csr_mats.train.pkl \
+--testdata_dump_path  dataset/ctr/criteo/train.txt.20170318_132217.discrete_50_26_contin_100000_2_0_xgb_0.all_split_by_field_csr_mats.test.pkl \
+> criteo_split_train_test3.log
+

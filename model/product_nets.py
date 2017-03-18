0624d2e1e6b4ce090a04136092de1735fe157934
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-03-18 17:38:37 +0800

    Add conf

--- model/product_nets.py
+++ model/product_nets.py
@@ -478,11 +478,12 @@ class RecIPNN(BaseModel):
         init_vars = []
         num_inputs = len(layer_sizes[0])
         factor_order = layer_sizes[1]
-        for i in range(num_inputs):
-            layer_input = layer_sizes[0][i]
-            layer_output = factor_order
-            init_vars.append(('w0_%d' % i, [layer_input, layer_output], 'tnormal', dtype))
-            init_vars.append(('b0_%d' % i, [layer_output], 'zero', dtype))
+        with tf.device('/cpu:0'):
+            for i in range(num_inputs):
+                layer_input = layer_sizes[0][i]
+                layer_output = factor_order
+                init_vars.append(('w0_%d' % i, [layer_input, layer_output], 'tnormal', dtype))
+                init_vars.append(('b0_%d' % i, [layer_output], 'zero', dtype))
         #add score bias for fields
         user_item_fields = [0,1]
         for i in user_item_fields:
@@ -532,16 +533,17 @@ class RecIPNN(BaseModel):
             w0 = [self.vars['w0_%d' % i] for i in range(num_inputs)]
             b0 = [self.vars['b0_%d' % i] for i in range(num_inputs)]
             #transform to embedding
-            embd_vecs = [tf.div(tf.sparse_tensor_dense_matmul(self.X[i], w0[i]),
-                                    tf.clip_by_value(tf.reshape(tf.sparse_reduce_sum(self.X[i],1),(-1,1)), 1.0, 999999.0))
-                             + b0[i]
-                            for i in range(num_inputs-prev_item_vec_cnt)]
-            #embd_vecs = [tf.sparse_tensor_dense_matmul(self.X[i], w0[i]) + b0[i] for i in range(num_inputs-prev_item_vec_cnt)]
-            if prev_item_vec_cnt != 0:
-                embd_vecs.extend([tf.div(tf.sparse_tensor_dense_matmul(self.X[i], w0[i]),
-                                         tf.clip_by_value(tf.reshape(tf.sparse_reduce_sum(self.X[i],1),(-1,1)), 1.0, 999999.0)
-                                         + b0[i])
-                                  for i in range(num_inputs)[-prev_item_vec_cnt:]])
+            with tf.device("/cpu:0"):
+                embd_vecs = [tf.div(tf.sparse_tensor_dense_matmul(self.X[i], w0[i]),
+                                        tf.clip_by_value(tf.reshape(tf.sparse_reduce_sum(self.X[i],1),(-1,1)), 1.0, 999999.0))
+                                 + b0[i]
+                                for i in range(num_inputs-prev_item_vec_cnt)]
+                #embd_vecs = [tf.sparse_tensor_dense_matmul(self.X[i], w0[i]) + b0[i] for i in range(num_inputs-prev_item_vec_cnt)]
+                if prev_item_vec_cnt != 0:
+                    embd_vecs.extend([tf.div(tf.sparse_tensor_dense_matmul(self.X[i], w0[i]),
+                                             tf.clip_by_value(tf.reshape(tf.sparse_reduce_sum(self.X[i],1),(-1,1)), 1.0, 999999.0)
+                                             + b0[i])
+                                      for i in range(num_inputs)[-prev_item_vec_cnt:]])
             l = tf.nn.dropout(
                 train_util.activate(
                     tf.concat(1, embd_vecs),

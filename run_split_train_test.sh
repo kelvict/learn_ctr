2c8dacfb6f1530431ab9f8235ed1c7b6e79acf7d
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-03-18 18:25:42 +0800

    Add conf

--- run_split_train_test.sh
+++ run_split_train_test.sh
@@ -14,7 +14,7 @@ python criteo.py --split_train_test --dataset_path dataset/ctr/criteo/train.txt.
 > criteo_split_train_test2.log
 '
 python criteo.py --split_train_test --dataset_path dataset/ctr/criteo/train.txt.20170318_132217.discrete_50_26_contin_100000_2_0_xgb_0.all_split_by_field_csr_mats.pkl \
---labels_path train.txt.20170318_132217.labels.txt \
+--labels_path dataset/ctr/criteo/train.txt.20170318_132217.labels.txt \
 --trainset_rate 0.9 \
 --traindata_dump_path dataset/ctr/criteo/train.txt.20170318_132217.discrete_50_26_contin_100000_2_0_xgb_0.all_split_by_field_csr_mats.train.pkl \
 --testdata_dump_path  dataset/ctr/criteo/train.txt.20170318_132217.discrete_50_26_contin_100000_2_0_xgb_0.all_split_by_field_csr_mats.test.pkl \

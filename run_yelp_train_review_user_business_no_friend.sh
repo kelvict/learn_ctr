4704ba53bd2e61b371a494d63ee1d6dbdad477a9
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-03-21 11:31:10 +0800

    conf

--- run_yelp_train_review_user_business_no_friend.sh
+++ run_yelp_train_review_user_business_no_friend.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-for conf_id in 2 3 1; do
-	for lr in 0.05; do
-		for width in 10 5; do
-			for n_embd in 100; do
-				for reg in 0.005 0.01; do
+for conf_id in 2 1 3; do
+	for lr in 0.0002; do
+		for width in 5; do
+			for n_embd in 50; do
+				for reg in 0.005; do
 					python criteo.py --ml --train  --gpu 1 \
 					--conf_path ./conf/yelp_RecIPNN_rate_review_user_business_no_friend_${conf_id}.conf \
 					--params --width ${width} --lr ${lr} --n_embd ${n_embd} --reg ${reg}\

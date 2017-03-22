3cd9822580f26964f20f84525611fd7c58d3a071
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-03-22 10:26:24 +0800

    Add conf

--- run_yelp_train_review_user_business.sh
+++ run_yelp_train_review_user_business.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-for conf_id in 1; do
-	for lr in 0.00005; do
-		for reg in 0.001 0.01; do
+for conf_id in 1 13 23; do
+	for lr in 0.0001; do
+		for reg in 0.001; do
 			for n_embd in 50; do
-				for width in 5 10; do
+				for width in 15 20; do
 					python criteo.py --ml --train  --gpu 0 \
 					--conf_path ./conf/yelp_RecIPNN_rate_review_user_business_${conf_id}.conf \
 					--params --width ${width} --lr ${lr} --n_embd ${n_embd} --reg ${reg}\

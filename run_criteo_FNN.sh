f5a007c5ec358c63651850fe7de4742e659b0959
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-05-04 15:11:31 +0800

    FB

--- run_criteo_FNN.sh
+++ run_criteo_FNN.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python criteo.py --train --conf_path ./conf/FNN.conf 1>criteo_train_FNN.log 2>&1
+python criteo.py --train --gpu 0 --conf_path ./conf/FNN.conf 1>criteo_train_FNN.log 2>&1

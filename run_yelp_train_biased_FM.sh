b9ed4addedc025bfc4b9ff403ca5c54d794dc82b
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-05-10 16:17:17 +0800

    Add config

---
+++ run_yelp_train_biased_FM.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+for conf_id in 1; do
+	for lr in 0.0002; do
+		for random_seed in `seq 0 0`; do
+			for trainset_rate in 0.9; do
+				for n_embd in 75 100 50; do #100 is best now
+					for reg in 0.001; do #0.001 is best now
+						python criteo.py --ml --train  --gpu 0 \
+						--conf_path ./conf/yelp_biased_MF_rate_${conf_id}.conf \
+						--params --lr ${lr} --n_embd ${n_embd} --reg ${reg}\
+						1>log/yelp_train_rate_real_biased_MF_${conf_id}.log 2>&1
+						sleep 2s
+					done
+				done
+			done
+		done
+	done
+done
